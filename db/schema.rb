cb74a3e77f5f5481c915a107fd90877d3cf6fa4d
L Lumbreras <llumbreras@gmail.com>
2013-08-09 16:10:52 -0700

    Create comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130806210436) do
+ActiveRecord::Schema.define(version: 20130809230912) do
 
   create_table "articles", force: true do |t|
     t.datetime "created_at"
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20130806210436) do
     t.string   "title"
     t.text     "body"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "author_name"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
